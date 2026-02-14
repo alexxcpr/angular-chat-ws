9f0c436c4a390cc9e59136a11622c0d236469900
alex.stanciulescu <alex.stanciulescu@onesoftware.ro>
2026-02-14 14:39:44 +0200

    html pentru chatScreen + instalare ng-zorro pentru componente + import CommonModule, FormsModule pentru input

--- client/src/app/pages/chat-screen/chat-screen.ts
+++ client/src/app/pages/chat-screen/chat-screen.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat-screen',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './chat-screen.html',
   styleUrl: './chat-screen.css',
 })
 export class ChatScreen {
-
+  // nevoie pentru html:
+  messages = []
+  draft = '';
+  send(): void{
+    console.log(`mesaj trimis: ${this.draft}`);
+    this.draft = '';
+  }
+  // message.text
 }
