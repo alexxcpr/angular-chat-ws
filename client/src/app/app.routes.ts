a44b91bcdf593c8ff68d8b8ba11005fbb9f64f16
alex.stanciulescu <alex.stanciulescu@onesoftware.ro>
2026-02-14 14:22:45 +0200

    metoda sendMessage + pagina pentru /chat (ChatScreen) + rutare catre /chat (manual /chat + implicit la root (/))

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ChatScreen } from './pages/chat-screen/chat-screen';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo: 'chat', pathMatch: 'full'},
+    {path:'chat', component: ChatScreen}
+];
--- client/src/app/core/services/chat-socket.ts
+++ client/src/app/core/services/chat-socket.ts
@@ -6,4 +6,13 @@ import { io, Socket } from 'socket.io-client';
 })
 export class ChatSocket {
   private socket: Socket = io('http:localhost:3000');
+
+  sendMessage(msg: string): void {
+    const msgTrimmed = msg.trim();
+    if (!msgTrimmed) return;
+
+    this.socket.emit('chat_message', msgTrimmed);
+  }
+
+  // onMessage() TODO
 }
