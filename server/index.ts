89f5c7ab1affeb14a2cf1521f20979ed2df0c941
alex.stanciulescu <alex.stanciulescu@onesoftware.ro>
2026-02-14 13:56:41 +0200

    conexiune backend la socket.io + flux gandire

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,19 @@
+import { Socket } from 'socket.io';
 import express from 'express'
 import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket: Socket) => {
+    console.log(`utilizatorul ${socket.id} s-a conectat`);
+    socket.on('disconnect', () => {
+        console.log(`utilizatorul ${socket.id} s-a deconectat`)
+    })
+})
+
 
 var port = 3000;
 server.listen(port, () => {
